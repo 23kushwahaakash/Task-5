e6669c09a47f5d6b3dbdd62ae7116438b7f5f016
Akash Kushwaha <23kushwahaakash@gmail.com>
2025-11-16 00:04:43 +0530

    Added recent jobs available

---
+++ teamTask/src/JobPage/Components/Jobcards.jsx
@@ -0,0 +1,48 @@
+import Briefcase from "../Images/Briefcase.png"
+import Clock from "../Images/Clock.png"
+import Wallet from "../Images/Wallet.png"
+import MapPin from "../Images/MapPin.png"
+import Save from "../Images/Save.png"
+
+function Jobcards({time,logo,title,position,duration,salary,location}) {
+  return (
+    <div className='shadow-lg rounded-lg grid grid-rows-3 p-2 md:p-5 gap-2 md:mb-5'>
+
+      <div className="flex justify-between items-center">
+        <h1 className="bg-gray-300 rounded-md px-1 font-serif text-gray-500 text-xs">{time}</h1>
+        <img src={Save} alt="save" className="h-5 w-5"/>
+      </div>
+
+      <div className="flex gap-1 md:gap-2 items-center">
+        <img src={logo} alt="logo" />
+        <h1 className="font-serif text-xl">{title}</h1>
+      </div>
+
+      <div className="flex">
+        <div className="hidden md:flex gap-5 w-full">
+          <div className="md:flex gap-1 items-center">
+            <img src={Briefcase} alt="briefcase" className="md:w-5 md:h-5" />
+            <h1 className="text-xs text-gray-500">{position}</h1>
+          </div>
+          <div className="flex gap-1 items-center">
+            <img src={Clock} alt="clock" />
+            <h1 className="text-xs text-gray-500">{duration}</h1>
+         </div>
+         <div className="flex gap-1 items-center">
+          <img src={Wallet} alt="wallet" />
+          <h1 className="text-xs text-gray-500">{salary}</h1>
+         </div>
+         <div className="flex gap-1 items-center">
+          <img src={MapPin} alt="Map pin" />
+          <h1 className="text-xs text-gray-500">{location}</h1>
+         </div>
+        </div>
+        <button className="bg-[#15294B] text-white md:text-xs mx-auto px-5 md:px-5 rounded-lg whitespace-nowrap">Job Details</button>
+      </div>
+
+            
+    </div>
+  )
+}
+
+export default Jobcards
--- teamTask/src/JobPage/Pages/JobPage.jsx
+++ teamTask/src/JobPage/Pages/JobPage.jsx
@@ -1,7 +1,13 @@
 import Map from "../Images/Map.png"
+import Google from "../Images/Google.png"
+import  Spotify from "../Images/Spotify.png"
+import Adobe from "../Images/Adobe.png"
+import Shopify from "../Images/Shopify.png"
+import Coursera from "../Images/Coursera.png"
 import HeroImage from "../Images/HeroImage.png"
 import Header from '../../LandingPage/Components/Header'
 import Footer from '../../LandingPage/Components/Footer'
+import Jobcards from "../Components/Jobcards"
 
 function JobPage() {
   return (
@@ -11,8 +17,61 @@ function JobPage() {
             <img src={Map} alt="map" className="relative md:min-h-screen "/>
             <img src={HeroImage} alt="hero image" className="absolute top-0 md:min-h-screen w-full pt-10 md:pt-20" />  
         </div>
-        <div cl>
-            <h1 className="font-serif ">Recent Jobs Available</h1>
+        <div className="md:p-5">
+            <h1 className="font-serif text-xl p-2 md:pt-5 md:text-4xl">Recent Jobs Available</h1>
+
+            <div className="grid grid-cols-2 p-5">
+                <div>
+                    <h1>Search by Job Title</h1>
+                </div>
+                <div className="flex flex-col">
+                    <Jobcards
+                    time="10 min ago"
+                    logo={Google}
+                    title="Google"
+                    position="Senior Product Designer"
+                    duration="Full time"
+                    salary="$40000-$42000"
+                    location="New-Delhi"/>
+
+                    <Jobcards
+                    time="12 min ago"
+                    logo={Spotify}
+                    title="Spotify"
+                    position="Associate Content Strategist"
+                    duration="Part time"
+                    salary="$28000-$32000"
+                    location="Greater-Noida"/>
+
+                    <Jobcards
+                    time="12 min ago"
+                    logo={Adobe}
+                    title="Adobe"
+                    position="UI/UX Designer"
+                    duration="Part time"
+                    salary="$85000-$115,000"
+                    location="Greater Noida"/>
+
+                    <Jobcards
+                    time="15 min ago"
+                    logo={Shopify}
+                    title="Shopify"
+                    position="Partner Marketing Manager"
+                    duration="Part time"
+                    salary="$25000-$82000"
+                    location="New-Delhi"/>
+
+                    <Jobcards
+                    time="20 min ago"
+                    logo={Coursera}
+                    title="Coursera"
+                    position="University Partnership Associate"
+                    duration="Part time"
+                    salary="$40000-$42000"
+                    location="Lucknow"/>
+                </div>
+
+            </div>
         </div>
         
         <Footer/>
