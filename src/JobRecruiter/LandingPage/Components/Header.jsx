63b6faf077ff8fb7d71ad8546d656a84cec91088
Akash Kushwaha <23kushwahaakash@gmail.com>
2025-12-17 02:28:33 +0530

    Resolved error of jobgiver by changing the spellings

--- src/JobRecruiter/LandingPage/Components/Header.jsx
+++ src/JobRecruiter/LandingPage/Components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"; 
 import { useNavigate } from "react-router-dom";
-import NextStep from "../Images/logo.svg";
+import NextStep from "../images/logo.svg";
 import Humburger from "./Humburger";
 import SmallerTabs from "./SmallerTabs";
 import ProfilePopUp from "./ProfilePopUp"
